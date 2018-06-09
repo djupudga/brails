e93dff730328e8ccc079e0c40821336d4024805e
Helgi Kristjansson <helgi.kristjansson@gmail.com>
2018-06-10 01:24:21 +0200

    Unfinished install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd $HOME
+mkdir .brails
+git clone https://github.com/djupudga/brails.git .brails
+cd .brails
+npm install
+npm link
+# if $SHELL is zsh
+#echo "PATH=$PATH:$HOME/.brails/bin" >> $HOME/.zshrc
+# else assume bash
+echo "Brails installed"
+echo "Paste this in your shell to use it now:"
+echo "export PATH=$PATH:$HOME/.brails/bin"
